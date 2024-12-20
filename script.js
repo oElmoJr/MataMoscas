28e21beba763c7add0e120df8a2cd9220a03895d
ElmoJr <72280546+oElmoJr@users.noreply.github.com>
2024-12-20 17:48:59 -0300

    First commit and create the adjustable screen dimensions.

---
+++ script.js
@@ -0,0 +1,11 @@
+let altura = 0;
+let largura = 0;
+
+function ajustarTamnhoPalcoJogo() {
+    altura = window.innerHeight;
+    largura = window.innerWidth;
+    
+    console.log(altura, largura);
+}
+
+ajustarTamnhoPalcoJogo() 
